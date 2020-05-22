76deb21adcd8df1904935cd744fa2e37562f6d15
quintin <qvlohuizen@live.nl>
2020-05-22 17:56:37 +0200

    running a model can be done with settings from the run.yaml

--- main.py
+++ main.py
@@ -4,17 +4,32 @@ from tensorflow.keras.datasets import mnist
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Flatten
 from tensorflow.keras.layers import Conv2D, MaxPooling2D
+from tensorflow.keras.callbacks import CSVLogger
 from tensorflow.keras import backend as K
 from parameters import Parameters
 from utils import *
+import os
+import numpy as np
+import json
+import yaml
+
+
+
+with open("runs/run.yaml") as file:
+    # The FullLoader parameter handles the conversion from YAML
+    # scalar values to Python the dictionary format
+    settings = yaml.load(file)
+
+    print(settings)
+
 
 #load all parameters into an object
-p = Parameters()
-
+p = Parameters(settings)
 
 # the data, split between train and test sets
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
+#fix channel
 if K.image_data_format() == "channels_first":
     x_train = x_train.reshape(x_train.shape[0], 1, p.img_rows, p.img_cols)
     x_test = x_test.reshape(x_test.shape[0], 1, p.img_rows, p.img_cols)
@@ -24,6 +39,7 @@ else:
     x_test = x_test.reshape(x_test.shape[0], p.img_rows, p.img_cols, 1)
     input_shape = (p.img_rows, p.img_cols, 1)
 
+#conversion, normalization, and reshaping
 x_train = x_train.astype("float32")
 x_test = x_test.astype("float32")
 x_train /= 255
@@ -36,6 +52,8 @@ print(x_test.shape[0], "test samples")
 y_train = tensorflow.keras.utils.to_categorical(y_train, p.num_classes)
 y_test = tensorflow.keras.utils.to_categorical(y_test, p.num_classes)
 
+
+#define a simple model
 model = Sequential()
 model.add(Conv2D(32, kernel_size=(3, 3), activation="relu", input_shape=input_shape))
 model.add(Conv2D(64, (3, 3), activation="relu"))
@@ -52,17 +70,30 @@ model.compile(
     metrics=["accuracy"],
 )
 
-model.fit(
+#create a csv logger
+csv_logger = CSVLogger(p.full_path_of_history)
+
+# Training
+actual_begin_time = datetime.now()
+history = model.fit(
     x_train,
     y_train,
+    callbacks=[csv_logger],
     batch_size=p.batch_size,
     epochs=p.epochs,
     verbose=1,
     validation_data=(x_test, y_test),
 )
+end_time = datetime.now()
+print("\nTraining time: {}\n".format(end_time - actual_begin_time))
+
+#eval model
 score = model.evaluate(x_test, y_test, verbose=0)
-print("Test loss:", score[0])
-print("Test accuracy:", score[1])
 
+#some prints
+print("\nTest loss:".format(str(score[0])))
+print("Test accuracy: {}\n".format(score[1]))
 
-
+#saving the model weights
+model.save_weights(p.full_path_of_weights)
+print("\nsaved model weights to: {}\n".format(p.full_path_of_weights))
--- parameters.py
+++ parameters.py
@@ -1,9 +1,37 @@
+from utils import *
+import os
+
 class Parameters(object):
-    def __init__(self):
-        self.batch_size = 16
-        self.num_classes = 10
-        self.epochs = 3
-        self.string_id = "first_model"
-        self.weights_ext = ".h5"
-        self.img_rows = 28
-        self.img_cols = 28
+    def __init__(self, settings):
+        #model parameters
+        self.model_name             = settings["model_name"]        #a unique name for this configuration, change to something unique
+        self.batch_size             = settings["batch_size"]
+        self.num_classes            = settings["num_classes"]
+        self.epochs                 = settings["epochs"]
+        self.img_rows               = settings["img_rows"]
+        self.img_cols               = settings["img_cols"]
+
+        #path stuff
+        self.root_dir_models        = settings["root_dir_models"]
+        self.model_folder           = get_time_string()     #A model with be stored in a folder with just a date&time as folder name
+        self.model_path             = os.path.join(self.root_dir_models, self.model_folder)     #path of model
+        self.make_model_dir()       #create directory for all data concerning this model.
+        
+        #weights .h5 file
+        self.weights_extension      = ".h5"                 #Extension for saving weights
+        self.filename_weights       = self.model_name + "_weights_only" + self.weights_extension
+        self.full_path_of_weights   = os.path.join(self.model_path, self.filename_weights)
+
+        #csv logger file to store the callback of the .fit function. It stores the history of the training session.
+        self.history_extension      = ".log"                 #Extension for history callback
+        self.filename_history       = self.model_name + "_history" + self.history_extension
+        self.full_path_of_history   = os.path.join(self.model_path, self.filename_history)
+
+
+    def make_model_dir(self):
+        try:
+            os.mkdir(self.model_path)
+        except OSError:
+            print ("Creation of the directory %s failed" % self.model_path)
+        else:
+            print ("Successfully created the directory %s " % self.model_path)
